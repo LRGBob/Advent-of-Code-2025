fd0691e7f0734abb6e1f54b558c060aed7123b43
Rashid <kushtov.rashid@gmail.com>
2026-04-20 15:54:28 -0400

    origin

--- day1_puzzle.py
+++ day1_puzzle.py
@@ -1,42 +1,43 @@
-f = open('puzzle_sample.txt', 'r')
+f = open('puzzle_input.txt', 'r')
 lines = f.readlines()
+items = list(range(100))
+length = len(items)
 starting_value = 50
 count = 0
 for instruction in lines:
     rotation = instruction.strip().split()
-    print(rotation)
     for char in rotation:
         if len(char) == 2:
             if char[0] == 'R':
-                value = int(str(char[1]))
-                starting_value += value
+                value = int(str(char[1]))       
+                starting_value = (starting_value + value) % length
                 if starting_value == 0:
-                    count += 1
+                    count += 1 
             elif char[0] == 'L':
                 value = int(str(char[1]))
-                starting_value -= value
+                starting_value = (starting_value - value) % length
                 if starting_value == 0:
-                    count += 1            
-        elif len(char) == 3:
+                    count += 1        
+        if len(char) == 3:
             if char[0] == 'R':
-                value = int(str(char[1]) + str(char[2]))
-                starting_value += value
+                value = int(str(char[1])+str(char[2]))    
+                starting_value = (starting_value + value) % length
                 if starting_value == 0:
-                    count += 1              
+                    count += 1             
             elif char[0] == 'L':
-                value = int(str(char[1]) + str(char[2]))
-                starting_value -= value
+                value = int(str(char[1])+str(char[2]))      
+                starting_value = (starting_value - value) % length
                 if starting_value == 0:
-                    count += 1
-        elif len(char) == 4:
+                    count += 1 
+        if len(char) == 4:
             if char[0] == 'R':
-                value = int(str(char[1]) + str(char[2]) + str(char [3]))
-                starting_value += value
+                value = int(str(char[1])+str(char[2])+str(char[3]))    
+                starting_value = (starting_value + value) % length
                 if starting_value == 0:
-                    count += 1
+                    count += 1             
             elif char[0] == 'L':
-                value = int(str(char[1]) + str(char[2]) + str(char [3]))
-                starting_value -= value
+                value = int(str(char[1])+str(char[2])+str(char[3]))      
+                starting_value = (starting_value - value) % length
                 if starting_value == 0:
                     count += 1
 print(count)
--- iterating_over_list.py
+++ iterating_over_list.py
@@ -2,12 +2,45 @@ items = list(range(100))
 length = len(items)
 
 starting_value = 50
-value = [55, 33]
+myList = ['L55','R5','R10','L9','L1','R199','R1']
+count = 0
 
-for step in value:
-    starting_value = (starting_value + step) % length
-    print(starting_value)
-
+for step in myList:
+    if len(step) == 2:
+        if step[0] == 'R':
+            value = int(str(step[1]))       
+            starting_value = (starting_value + value) % length
+            if starting_value == 0:
+                count += 1 
+        elif step[0] == 'L':
+            value = int(str(step[1]))
+            starting_value = (starting_value - value) % length
+            if starting_value == 0:
+                count += 1        
+    if len(step) == 3:
+        if step[0] == 'R':
+            value = int(str(step[1])+str(step[2]))    
+            starting_value = (starting_value + value) % length
+            if starting_value == 0:
+                count += 1             
+        elif step[0] == 'L':
+            value = int(str(step[1])+str(step[2]))      
+            starting_value = (starting_value - value) % length
+            if starting_value == 0:
+                count += 1 
+    if len(step) == 4:
+        if step[0] == 'R':
+            value = int(str(step[1])+str(step[2])+str(step[3]))    
+            starting_value = (starting_value + value) % length
+            if starting_value == 0:
+                count += 1             
+        elif step[0] == 'L':
+            value = int(str(step[1])+str(step[2])+str(step[3]))      
+            starting_value = (starting_value - value) % length
+            if starting_value == 0:
+                count += 1
+print(starting_value)
+print(count)
 # # Forward movement
 # for i in range(length):
 #     print(items[i % length]) # Results: a, b, c, a, b, c...
